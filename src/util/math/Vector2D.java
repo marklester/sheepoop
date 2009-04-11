9559a0dd2cbbf09547d2f37292d663f484ce065f
philfreo <philfreo@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-11 06:09:45 +0000

    forgot a class

---
+++ src/util/math/Vector2D.java
@@ -0,0 +1,31 @@
+package util.math;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author Phil Freo
+ */
+public class Vector2D implements Serializable {
+	private static final long serialVersionUID = 1398372928692423286L;
+	private float x;
+	private float y;
+
+	public Vector2D(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public float getMagnitude() {
+		return (float) Math.sqrt(Math.pow(getX(), 2) + Math.pow(getY(), 2));
+	}
+	
+}
