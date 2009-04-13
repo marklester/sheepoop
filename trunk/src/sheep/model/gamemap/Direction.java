bc2704bd9efca388028f177bcc4621dfc74c867e
BryanRosander <BryanRosander@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-13 23:41:58 +0000

    Direction takes into account Hex Map

--- trunk/src/sheep/model/gamemap/Direction.java
+++ trunk/src/sheep/model/gamemap/Direction.java
@@ -6,23 +6,35 @@ import sheep.util.math.Vector2D;
  * 
  * @author Phil Freo
  */
+
 public enum Direction {
-	N(new Vector2D(0, 1)), 
-	NE(new Vector2D(1, 1)), 
-	E(new Vector2D(0, 1)), 
-	SE(new Vector2D(1, -1)), 
-	S(new Vector2D(0, -1)), 
-	SW(new Vector2D(-1, -1)),
-	W(new Vector2D(-1, 0)),
-	NW(new Vector2D(-1, 1));
+	N(new Vector2D(0, -1), new Vector2D(0,-1)), 
+	NE(new Vector2D(1, 0), new Vector2D(1, -1)), 
+	SE(new Vector2D(1,1), new Vector2D(1,0)), 
+	S(new Vector2D(0, 1), new Vector2D(0,1)), 
+	SW(new Vector2D(-1, 1), new Vector2D(-1,0)),
+	NW(new Vector2D(-1, 0), new Vector2D(-1,-1));
+	
+	
 
-	private Vector2D vector;
+	
+	private Vector2D oddVector;
+	private Vector2D evenVector;
 
-	Direction(Vector2D vec) {
-		this.vector = vec;
+	Direction(Vector2D oddVec,Vector2D evenVec) {
+		this.oddVector = oddVec;
+		this.evenVector = evenVec;
 	}
 
-	public Vector2D getVector() {
-		return vector;
+	public Vector2D getVector(Location l) {
+		int x = l.getX();
+		if(x%2==1)
+		{
+			return oddVector;
+		}
+		else
+		{
+			return evenVector;
+		}
 	}
 }
