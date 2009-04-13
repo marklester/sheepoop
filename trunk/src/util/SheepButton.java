210199ae3a88b496e2696c02d78a50f29a45dd0d
jmoney38 <jmoney38@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-13 23:55:30 +0000

    transparent buttons

--- trunk/src/util/SheepButton.java
+++ trunk/src/util/SheepButton.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
@@ -10,6 +11,8 @@ public class SheepButton extends JButton {
 	public SheepButton(ImageIcon icon, AbstractAction act) {
 		super(icon);
 		this.addActionListener(act);
+		this.setOpaque(false);
+		this.setBackground(Color.white);
 	}
 	
 	public SheepButton(ImageIcon icon, AbstractAction act, Dimension d) {
