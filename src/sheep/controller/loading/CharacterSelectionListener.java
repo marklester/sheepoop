390aaec7f42a463b41154ef74f61c23f8fe793dc
b3tamike <b3tamike@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-14 03:55:28 +0000

    got the welcomeview hooked up with the actionlisteners

--- src/sheep/controller/loading/CharacterSelectionListener.java
+++ src/sheep/controller/loading/CharacterSelectionListener.java
@@ -3,12 +3,23 @@ package sheep.controller.loading;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import sheep.view.loading.WelcomeView;
+
 public class CharacterSelectionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
+		String command = e.getActionCommand();
+		
+		if( command.equals( WelcomeView.SMASHER) ) {
+			System.out.println(command);
+		} 
+		else if( command.equals( WelcomeView.SUMMONER) ) {
+			System.out.println(command);
+		}
+		else if( command.equals( WelcomeView.SNEAK) ) {
+			System.out.println(command);
+		}
 	}
 
 }
