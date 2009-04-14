417089b9f008a367eb818fd3e3a01f8f299eba59
BryanRosander <BryanRosander@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-14 03:18:33 +0000

    Implemented the occupations... they'll have to be initialized with a character

--- trunk/src/sheep/model/occupations/Summoner.java
+++ trunk/src/sheep/model/occupations/Summoner.java
@@ -1,12 +1,29 @@
 package sheep.model.occupations;
 
+import sheep.model.entities.StatType;
+import sheep.model.skills.BindWounds;
+import sheep.model.skills.Observation;
+import sheep.model.skills.PassiveSkill;
+import sheep.model.entities.Character;
 
 public class Summoner extends Occupation {
 	
 	private static final long serialVersionUID = -7063196676299952873L;
 
-	public Summoner() {
-		super(null,null,null,null);
-		throw new UnsupportedOperationException();
+	public Summoner(Character c) {
+		super("Smasher");
+		setInitialCharacterStat(StatType.AGILITY, 10);
+		setInitialCharacterStat(StatType.HARDINESS, 10);
+		setInitialCharacterStat(StatType.INTELLECT, 30);
+		setInitialCharacterStat(StatType.SPEED, 10);
+		setInitialCharacterStat(StatType.STRENGTH, 10);
+		setInitialCharacterStat(StatType.LIVES_LEFT, 5);
+		addPerformableSkill(new Observation(c));
+		addPerformableSkill(new BindWounds(c));
+		setInitialPassiveSkill(PassiveSkill.BARGAIN, 0);
+		setInitialPassiveSkill(PassiveSkill.BANE, 0);
+		setInitialPassiveSkill(PassiveSkill.BOON, 0);
+		setInitialPassiveSkill(PassiveSkill.ENCHANTMENT, 0);
+		setInitialPassiveSkill(PassiveSkill.STAFF, 0);
 	}
 }
