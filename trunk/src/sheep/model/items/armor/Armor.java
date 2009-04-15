0e17f6f4e05e8e71500397fcfca040e0f25ae937
BryanRosander <BryanRosander@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-15 23:37:08 +0000

    implemented armor, steelwool

--- trunk/src/sheep/model/items/armor/Armor.java
+++ trunk/src/sheep/model/items/armor/Armor.java
@@ -2,27 +2,39 @@ package sheep.model.items.armor;
 
 import sheep.model.entities.BodyPart;
 import sheep.model.entities.Entity;
+import sheep.model.entities.StatType;
 import sheep.model.gamemap.GameMap;
 import sheep.model.gamemap.Location;
 import sheep.model.items.Takeable;
+import sheep.model.entities.Character;
 
 public abstract class Armor extends Takeable {
+	
+	private int armorBonus;
 	
 	private static final long serialVersionUID = 6475799713135184079L;
 	private BodyPart where;
 	
-	public Armor(String id, GameMap map, Location loc, BodyPart bodyLocation) {
+	public Armor(String id, GameMap map, Location loc, BodyPart bodyLocation, int armorBonus) {
 		super("Armor", map, loc);
 		where = bodyLocation;
+		this.armorBonus = armorBonus;
 	}
 	
 	public void use(Entity entity)
 	{
 		entity.equip(this);
 	}
-	
 	public BodyPart getBodyPart() {
 		return this.where;
 	}
-
+	public void equipTo(Character c)
+	{
+		c.affectStat(StatType.ARMOR_RATING, armorBonus);
+	}
+	
+	public void unequipFrom(Character c)
+	{
+		c.affectStat(StatType.ARMOR_RATING, -armorBonus);
+	}
 }
--- trunk/src/sheep/model/items/armor/SteelWool.java
+++ trunk/src/sheep/model/items/armor/SteelWool.java
@@ -1,16 +1,15 @@
 package sheep.model.items.armor;
 
 import sheep.model.entities.BodyPart;
-import sheep.model.entities.Entity;
 import sheep.model.gamemap.GameMap;
 import sheep.model.gamemap.Location;
 
 public class SteelWool extends Armor
 {
+	private static final long serialVersionUID = 1L;
 
 	public SteelWool(GameMap map, Location loc)
 	{
-		super("Steel Wool", map, loc, BodyPart.CHEST);
-		// TODO Auto-generated constructor stub
+		super("Steel Wool", map, loc, BodyPart.CHEST, 30);
 	}
 }
