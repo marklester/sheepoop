d7f096cc15033a0466b18c73a162c44f89149c8c
mlester <mlester@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-15 14:39:39 +0000

    implemented DumbAI moves N and S at randomly.

--- trunk/src/sheep/model/entities/npc/DumbAI.java
+++ trunk/src/sheep/model/entities/npc/DumbAI.java
@@ -1,16 +1,30 @@
 package sheep.model.entities.npc;
 
+import sheep.model.Time;
+import sheep.model.gamemap.Direction;
 import sheep.model.gamemap.GameMap;
 
 public class DumbAI extends AI {
 
 	private static final long serialVersionUID = 3836274916586137446L;
-
+	private int state;
 	public DumbAI(NPC npc, GameMap map) {
 		super(npc, map);
+		state=0;
+		Time.getInstance().registerObserver(this);
 	}
 
 	public void tick() {
-
+		state = (int)(Math.random()*8);
+		if(this.getNPC().isMoving()){
+			if(state<4){}else{
+				this.getNPC().stopMoving();
+			}
+		}else{
+			if(state<4)
+				this.getNPC().startMoving(Direction.S);
+			else
+				this.getNPC().startMoving(Direction.N);
+		}
 	}
 }
