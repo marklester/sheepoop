56f580aa296447f5e430ef6ac71f1cb453d42352
BryanRosander <BryanRosander@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-16 04:13:08 +0000

    fixed typo

--- trunk/src/sheep/model/items/weapons/Lightning.java
+++ trunk/src/sheep/model/items/weapons/Lightning.java
@@ -3,11 +3,11 @@ package sheep.model.items.weapons;
 import sheep.model.gamemap.GameMap;
 import sheep.model.gamemap.Location;
 
-public class Lightening extends Bane {
+public class Lightning extends Bane {
 	private static final long serialVersionUID = 5708249318652445L;
 
-	public Lightening(GameMap map, Location loc) {
-		super("Lightening", map, loc, 15,2);
+	public Lightning(GameMap map, Location loc) {
+		super("Lightning", map, loc, 15,2);
 	}
 
 
