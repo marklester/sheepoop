d232e4fefdbacdef1c33ee42c161154c56d55977
philfreo <philfreo@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-18 05:37:05 +0000

    fixed nunchucks

--- trunk/src/sheep/model/items/weapons/Nunchucks.java
+++ trunk/src/sheep/model/items/weapons/Nunchucks.java
@@ -1,14 +1,15 @@
 package sheep.model.items.weapons;
 
-import sheep.model.gamemap.GameMap;
+import sheep.model.Model;
 import sheep.model.gamemap.Location;
+
 
 public class Nunchucks extends OneHanded {
 
 	private static final long serialVersionUID = -5514383778390944563L;
 	
-	public Nunchucks(GameMap map, Location loc) {
-		super("Nunchucks", map, loc, 8);
+	public Nunchucks(Model model, Location loc) {
+		super("Nunchucks", model, loc, 8);
 	}
 	
 }
