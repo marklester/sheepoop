239db42107a8305e7142fef2133334140e4b8c09
mlester <mlester@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-18 18:25:31 +0000

    made detect trap work

--- src/sheep/model/skills/RemoveTrap.java
+++ src/sheep/model/skills/RemoveTrap.java
@@ -1,6 +1,13 @@
 package sheep.model.skills;
 
 import java.awt.event.ActionEvent;
+import java.util.Vector;
+
+import sheep.model.gamemap.Direction;
+import sheep.model.gamemap.Locatable;
+import sheep.model.gamemap.Location;
+import sheep.model.items.Trap;
+import sheep.util.math.Vector2D;
 
 public class RemoveTrap extends PerformableSkill {
 	
@@ -10,6 +17,24 @@ public class RemoveTrap extends PerformableSkill {
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		int skill = getCharacter().getSkill(PassiveSkill.DETECT_TRAP);
+		int chance = skill - Math.abs((int)(Math.random()*(10-skill)));
+		Direction facingDirection = getCharacter().getFacingDirection();
+		Vector2D vector = facingDirection.getVector(getCharacter().getLocation());
+		Location newLoc = getCharacter().getLocation().addVector(vector);
+
+		// Get neighboring locatables
+		Vector<Locatable> thingsOnTile = getCharacter().getGameMap().get(newLoc);
+		int i=0;
+		for(Locatable loc:thingsOnTile){
+			if(loc instanceof Trap){
+				if(chance>(int)((Trap) loc).getDmgAmt()/10){
+					((Trap) loc).deActivate();
+					getCharacter().hearMessage(getCharacter(), "Deactivated Trap");
+				}
+			}
+		}
+		if(i==0)getCharacter().hearMessage(getCharacter(), "Nothing There");
 	}
 
 }
