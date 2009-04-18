c5d1a928b737d2ea1efbb21845310337a38c8fac
jmoney38 <jmoney38@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-18 16:34:19 +0000

    Items can be equipped and unequipped from the inventory.

---
+++ src/sheep/controller/EquipmentActionListener.java
@@ -0,0 +1,35 @@
+package sheep.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import sheep.model.entities.Avatar;
+import sheep.model.entities.BodyPart;
+import sheep.model.items.Takeable;
+
+public class EquipmentActionListener implements ActionListener {
+
+	private final Avatar avatar;
+	private BodyPart where;
+	
+	//Constructs a listener for the Weapon of the avatar
+	public EquipmentActionListener(Avatar avatar) {
+		this.avatar = avatar;
+	}
+	
+	//Constructs a listener for the Armor at a specific body part of the avatar
+	public EquipmentActionListener(Avatar avatar, BodyPart where) {
+		this.avatar = avatar;
+		this.where = where;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (where == null) 
+			avatar.unequipWeapon();
+		else
+			avatar.unequipArmor(where);
+
+	}
+
+}
