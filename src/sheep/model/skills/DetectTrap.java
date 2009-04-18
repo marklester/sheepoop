e8a400bf8f5af255abbbfef09f85b12097380712
mlester <mlester@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-18 18:24:24 +0000

    added detect skill

---
+++ src/sheep/model/skills/DetectTrap.java
@@ -0,0 +1,36 @@
+package sheep.model.skills;
+
+import java.awt.event.ActionEvent;
+import java.util.Map;
+import java.util.Vector;
+import java.util.Map.Entry;
+import sheep.model.gamemap.Locatable;
+import sheep.model.gamemap.Location;
+import sheep.model.items.Trap;
+
+
+public class DetectTrap extends PerformableSkill {
+	
+	private static final long serialVersionUID = -6400640105850953368L;
+	public DetectTrap(){
+		super("Detect Trap");
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Location center = getCharacter().getLocation();
+		int radius = getCharacter().getSkill(PassiveSkill.DETECT_TRAP);
+		// Get neighboring locatables
+		getCharacter().hearMessage(getCharacter(), " is detecting Traps");
+		Map<Location, Vector<Locatable>> tiles = getCharacter().getGameMap().getMapSubset(center, radius);
+		for (Entry<Location, Vector<Locatable>> entry : tiles.entrySet()) {
+			Location loc = entry.getKey();
+			Vector<Locatable> locatables = entry.getValue();
+			for(Locatable l : locatables){
+				if(l instanceof Trap){
+					((Trap) l).showTrap();
+				}
+			}
+		}
+	}
+
+}
