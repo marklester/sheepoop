81dcab4961ae5785bd72a7919fd497ac9b0ff26f
BryanRosander <BryanRosander@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-18 06:20:24 +0000

    AngryAI now just subtracts x and y to see if you're in its range

--- trunk/src/sheep/model/entities/npc/AngryAI.java
+++ trunk/src/sheep/model/entities/npc/AngryAI.java
@@ -14,7 +14,7 @@ import sheep.model.gamemap.Location;
 public class AngryAI extends AI {
 
 	private static final long serialVersionUID = -576367827163651816L;
-	
+
 	public AngryAI(NPC npc, Model model) {
 		super(npc, model);
 	}
@@ -24,53 +24,42 @@ public class AngryAI extends AI {
 		Direction dirToAvatar = null;
 		Location avatarLoc = this.getModel().getAvatar().getLocation();	// I'm too good to care about LoD!
 		Location npcLoc = this.getNPC().getLocation();
-		
-		// Only try to find avatar if its within radius of 5
-		HashMap<Location, Vector<Locatable>> neighbors = this.getGameMap().getMapSubset(npcLoc, 5);
-		boolean avatarFound = false;
-		for (Entry<Location, Vector<Locatable>> locatables : neighbors.entrySet()) {
-			for (Locatable locatable : locatables.getValue()) {			
-				if (locatable == this.getModel().getMover()) {
-					avatarFound = true;
-				}
-			}
-		}
-		if (!avatarFound) {
-			return;
-		}
-		
-		if (Math.abs(avatarLoc.getX() - npcLoc.getX()) > Math.abs(avatarLoc.getY() - npcLoc.getY())) {
-			// move left or right
-			if (avatarLoc.getX() < npcLoc.getX()) { 
-				// avatar is left
-				if (avatarLoc.getY() < npcLoc.getY()) {
-					// avatar is up
-					dirToAvatar = Direction.NW;
+
+		if(Math.abs(avatarLoc.getX() - npcLoc.getX())<4&& Math.abs(avatarLoc.getY() - npcLoc.getY())<4)
+		{
+			if (Math.abs(avatarLoc.getX() - npcLoc.getX()) > Math.abs(avatarLoc.getY() - npcLoc.getY())) {
+				// move left or right
+				if (avatarLoc.getX() < npcLoc.getX()) { 
+					// avatar is left
+					if (avatarLoc.getY() < npcLoc.getY()) {
+						// avatar is up
+						dirToAvatar = Direction.NW;
+					} else {
+						// avatar is down
+						dirToAvatar = Direction.SW;
+					}
 				} else {
-					// avatar is down
-					dirToAvatar = Direction.SW;
+					// avatar is right
+					if (avatarLoc.getY() < npcLoc.getY()) {
+						// avatar is up
+						dirToAvatar = Direction.NE;
+					} else {
+						// avatar is down
+						dirToAvatar = Direction.SE;
+					}
 				}
 			} else {
-				// avatar is right
 				if (avatarLoc.getY() < npcLoc.getY()) {
 					// avatar is up
-					dirToAvatar = Direction.NE;
+					dirToAvatar = Direction.N;
 				} else {
 					// avatar is down
-					dirToAvatar = Direction.SE;
+					dirToAvatar = Direction.S;
 				}
 			}
-		} else {
-			if (avatarLoc.getY() < npcLoc.getY()) {
-				// avatar is up
-				dirToAvatar = Direction.N;
-			} else {
-				// avatar is down
-				dirToAvatar = Direction.S;
-			}
+
+			this.getNPC().startMoving(dirToAvatar);
 		}
-		
-		this.getNPC().startMoving(dirToAvatar);
 	}
 
 	@Override
