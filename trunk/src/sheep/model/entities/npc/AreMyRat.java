c5871292b22cf49a8708041a5d26b76a1a7c206e
mlester <mlester@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-18 21:41:28 +0000

    villager npc

---
+++ trunk/src/sheep/model/entities/npc/AreMyRat.java
@@ -0,0 +1,29 @@
+package sheep.model.entities.npc;
+
+import sheep.model.Model;
+import sheep.model.entities.Character;
+import sheep.model.entities.StatType;
+import sheep.model.gamemap.Location;
+import sheep.model.occupations.Sneak;
+
+public class AreMyRat extends NPC {
+	private static final long serialVersionUID = 6747342338321148905L;
+	private String[] mytalks = {"Are My?"};
+	private int currenttalk =0;
+	public AreMyRat(Model model, Location loc) {
+		super("AreMyRat", model, loc, new Sneak());
+		setAi(new VillagerAI(this, model));
+		this.getStats().change(StatType.MONEY, 0);
+	}
+	public void talk(Character character) {
+		//System.out.println("Talk:"+currenttalk);
+		if(mytalks!=null&&currenttalk<mytalks.length){
+			character.hearMessage(this, mytalks[currenttalk]);
+		}
+		if(mytalks!=null&&currenttalk==mytalks.length){
+			currenttalk=0;
+		}else{
+			currenttalk++;
+		}
+	}
+}
