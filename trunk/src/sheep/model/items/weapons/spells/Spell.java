76558f5324637e770b3652994baaa3aa934f9206
BryanRosander <BryanRosander@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-18 22:54:03 +0000

    put the logic for projectiles in projectileweapon

--- trunk/src/sheep/model/items/weapons/spells/Spell.java
+++ trunk/src/sheep/model/items/weapons/spells/Spell.java
@@ -1,57 +1,18 @@
 package sheep.model.items.weapons.spells;
 
-import java.awt.event.ActionEvent;
-import java.util.List;
-
 import sheep.model.Model;
-import sheep.model.entities.Character;
-import sheep.model.entities.StatType;
-import sheep.model.gamemap.Locatable;
 import sheep.model.gamemap.Location;
-import sheep.model.items.weapons.Projectile;
-import sheep.model.items.weapons.Weapon;
+import sheep.model.items.weapons.ProjectileWeapon;
 import sheep.model.skills.PassiveSkill;
-import sheep.util.math.Vector2D;
 
-public abstract class Spell extends Weapon {
+public abstract class Spell extends ProjectileWeapon {
 	
 	int mySpeed;
 
 	private static final long serialVersionUID = -1606083256607931219L;
 	
 	public Spell(String id, Model model, Location loc, int baseDamage, PassiveSkill skill, int speed) {
-		super(id, model, loc, baseDamage, skill);
+		super(id,id, model, loc, baseDamage, speed, skill);
 		mySpeed = speed;
-	}
-	@Override
-	public void actionPerformed(ActionEvent ae) {
-		Character enemy = getUser().getInteractingCharacter();
-		
-		if( enemy != null )
-		{
-			int totalDamage = (int) Math.floor( (double) getBaseDamage() * (  (double) getUser().getSkill( getSkill() ) ) );
-			
-			enemy.affectStat( StatType.LIFE, totalDamage );
-		}
-		else
-		{
-			Vector2D myVector = getUser().getFacingDirection().getVector(getLocation());
-			Location attackingTile = new Location(getLocation().getX()+(int)myVector.getX(),getLocation().getY()+(int)myVector.getY());
-			List<Locatable> targets = getGameMap().get(attackingTile);
-			boolean blocked = false;
-			Projectile myProj = new Projectile(getID(),getModel(),attackingTile,this,getUser().getFacingDirection(),mySpeed);
-			for(Locatable l: targets)
-			{
-				l.hitWith(this);
-				if(l.blocks(myProj))
-				{
-					blocked = true;
-				}
-			}
-			if(!blocked)
-			{
-				getGameMap().add(attackingTile, myProj);
-			}
-		}
 	}
 }
