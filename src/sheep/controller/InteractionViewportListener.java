78815cdb5b4064b3423578e1cc993f88d475bf31
philfreo <philfreo@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-19 05:37:46 +0000

    trade window setup

--- src/sheep/controller/InteractionViewportListener.java
+++ src/sheep/controller/InteractionViewportListener.java
@@ -3,6 +3,11 @@ package sheep.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractAction;
+
+import sheep.controller.actions.TalkAction;
+import sheep.controller.actions.UseItemAction;
+import sheep.controller.actions.UseWeaponAction;
 import sheep.model.Model;
 import sheep.view.View;
 
@@ -21,13 +26,25 @@ public class InteractionViewportListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		//System.out.println("Inventory button was clicked: " + e.getActionCommand());
 		
+		// Which action
 		String cmd = e.getActionCommand();
+		AbstractAction action = null;
 		if (cmd.equalsIgnoreCase("attack")) {
-			
-		} else if(false) {
+			action = new UseWeaponAction(model.getAvatar());
+		} else if(cmd.equalsIgnoreCase("talk")) {
+			action = new TalkAction(model.getAvatar(), model.getAvatar().getInteractingCharacter());
+		} else if(cmd.equalsIgnoreCase("useItem")) {
+			action = new UseItemAction(model.getAvatar(), model.getAvatar().getInventory().get().iterator().next());
+		}  else if(cmd.equalsIgnoreCase("trade")) {
 			
 		} 
 		
+		// Execute action
+		if (action != null) {
+			action.actionPerformed(e);
+		}
+		
+		// Back to game play
 		view.toggleActionMenu();
 		model.startTime();
 		view.getLayeredPane().grabFocus();
