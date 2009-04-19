731fa960e3ce4f99c511321bb6e24b304fb19182
BryanRosander <BryanRosander@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-19 07:05:43 +0000

    enrage animal

--- src/sheep/model/gamemap/Level1MapCreator.java
+++ src/sheep/model/gamemap/Level1MapCreator.java
@@ -40,6 +40,7 @@ import sheep.model.items.weapons.ShortSword;
 import sheep.model.items.weapons.SpearGun;
 import sheep.model.items.weapons.Trident;
 import sheep.model.items.weapons.spells.CalmAnimal;
+import sheep.model.items.weapons.spells.EnrageAnimal;
 import sheep.model.items.weapons.spells.Fire;
 import sheep.model.occupations.Smasher;
 
@@ -462,6 +463,9 @@ public class Level1MapCreator extends MapCreator {
 		
 		loc = new Location (2,0);
 		map.add(loc, new CalmAnimal(model,loc));
+		
+		loc = new Location (3,0);
+		map.add(loc, new EnrageAnimal(model,loc));
 		
 		loc = new Location(16, 30);
 		map.add(loc, new Plane(model, loc, p));
