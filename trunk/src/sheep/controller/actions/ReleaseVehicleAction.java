85761a097b21c58da91d744e39404324f18da9b0
BryanRosander <BryanRosander@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-20 02:34:39 +0000

    fixed bug that let you move boat 1 tile further onto land

--- trunk/src/sheep/controller/actions/ReleaseVehicleAction.java
+++ trunk/src/sheep/controller/actions/ReleaseVehicleAction.java
@@ -23,7 +23,7 @@ public class ReleaseVehicleAction extends AbstractAction {
 	private static final long serialVersionUID = -1934297505909650891L;
 
 	private final Model model;
-	
+
 	public ReleaseVehicleAction(Model model) {
 		super();
 		this.model = model;
@@ -33,46 +33,61 @@ public class ReleaseVehicleAction extends AbstractAction {
 	public void actionPerformed(ActionEvent e) {
 		Entity mover = model.getMover();
 		Avatar avatar = model.getAvatar();
-		
+
 		// Do nothing if the avatar isn't in a vehicle
 		if (mover == avatar) {
 			return;
 		}
 		Vehicle vehicle = (Vehicle) mover;
-		
+
 		// Do nothing if the vehicle isn't currently next to a terrain the 
 		// avatar can't go on by itself
 		Map<Location, List<Locatable>> thingsOnMyTile = model.getGameMap().getMapSubset(vehicle.getLocation(), 1);
-		
+
 		// Look for a terrain next to vehicle that the avatar can get off
 		boolean avatarCanGetOff = true; 
 		Location passableLoc = null;
-		for (Entry<Location, List<Locatable>> locatables : thingsOnMyTile.entrySet()) {
-			avatarCanGetOff = true;
-			for (Locatable locatable : locatables.getValue()) {
-				passableLoc = locatable.getLocation();
-				if (locatable != vehicle && locatable != avatar && locatable.blocks(avatar)) {
-					avatarCanGetOff = false;
-					break;
-				}				
-			}
-			if(avatarCanGetOff)
-			{
+		for(Locatable l: model.getGameMap().get(mover.getLocation()))
+		{
+			if (l != vehicle && l != avatar && l.blocks(avatar)) {
+				avatarCanGetOff = false;
 				break;
+			}	
+		}
+		if(!avatarCanGetOff)
+		{
+			for (Entry<Location, List<Locatable>> locatables : thingsOnMyTile.entrySet()) {
+				avatarCanGetOff = true;
+				for (Locatable locatable : locatables.getValue()) {
+					passableLoc = locatable.getLocation();
+					if (locatable != vehicle && locatable != avatar && locatable.blocks(avatar)) {
+						avatarCanGetOff = false;
+						break;
+					}				
+				}
+				if(avatarCanGetOff)
+				{
+					break;
+				}
 			}
+
+			// Don't release vehicle if avatar can't get off
+			if (!avatarCanGetOff) {
+				return;
+			}
+
+			// Move vehicle (and avatar) to nearest passable tile
+			mover.setLocation(passableLoc);
+
+			// Release vehicle
+			vehicle.clearDriver();
+			model.setMover((Entity)model.getAvatar());
 		}
-		
-		// Don't release vehicle if avatar can't get off
-		if (!avatarCanGetOff) {
-			return;
+		else
+		{
+			vehicle.clearDriver();
+			model.setMover((Entity)model.getAvatar());
 		}
-		
-		// Move vehicle (and avatar) to nearest passable tile
-		mover.setLocation(passableLoc);
-		
-		// Release vehicle
-		vehicle.clearDriver();
-		model.setMover((Entity)model.getAvatar());
 	}
 
 }
