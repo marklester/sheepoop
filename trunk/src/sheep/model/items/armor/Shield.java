601885412b86b70202509d34f2690625c1201f3a
BryanRosander <BryanRosander@b38644e2-1b1c-11de-a027-6f5b8e30e268>
2009-04-20 03:04:02 +0000

    two handed weapons and range weapons cannot be equipped with shield

--- trunk/src/sheep/model/items/armor/Shield.java
+++ trunk/src/sheep/model/items/armor/Shield.java
@@ -2,7 +2,9 @@ package sheep.model.items.armor;
 
 import sheep.model.Model;
 import sheep.model.entities.BodyPart;
+import sheep.model.entities.Character;
 import sheep.model.gamemap.Location;
+import sheep.model.skills.PassiveSkill;
 
 public class Shield extends Armor
 {
@@ -11,5 +13,16 @@ public class Shield extends Armor
 	public Shield(Model model, Location loc)
 	{
 		super("Shield", model, loc, BodyPart.AUX, 25, 100);
+	}
+	
+	@Override
+	public void use(Character entity)
+	{
+		entity.equip(this);
+		PassiveSkill s = entity.getEquippedWeapon().getSkill();
+		if((s==PassiveSkill.TWO_HANDED_WEAPON)||(s==PassiveSkill.RANGED_WEAPON))
+		{
+			entity.unequipWeapon();
+		}
 	}
 }
